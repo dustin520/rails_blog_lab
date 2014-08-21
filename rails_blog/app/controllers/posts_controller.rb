00933813bae5c4081956b3e1c5514786f25f75f3
Dustin Yu <dustin520@gmail.com>
2014-08-20 20:21:25 -0700

    posts and tags connected, need to render by tag

--- rails_blog/app/controllers/posts_controller.rb
+++ rails_blog/app/controllers/posts_controller.rb
@@ -14,10 +14,11 @@ class PostsController < ApplicationController
 		new_post = (params[:post]).permit(:title, :author, :description)
 		post = Post.create(new_post)
 		new_tag = (params[:tag]).permit(:name)
-		tag = Tag.create(new_tag)
-		new_post_tag = (params[:post_tag]).permit(:date)
-		post_tag = Post_tag.create(new_post_tag.post_id)
-		post_tag.tag_id << post_tag
+		tag = Tag.find_or_create_by(name: new_tag[:name]) #???
+		post.tags << tag
+		# new_post_tag = (params[:post_tag]).permit(:date)
+		# post_tag = Post_tag.create(new_post_tag.post_id)
+		# post_tag.tag_id << post_tag
 		redirect_to "/posts"
 	end
 
