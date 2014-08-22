76f4f9be7201e4f25d6fa58338edbc951af9ba35
Dustin Yu <dustin520@gmail.com>
2014-08-22 11:34:35 -0700

    add testing

---
+++ rails_blog/spec/models/student_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Student, :type => :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  ### Change below to do posts
+ #  it "should create new student w/o saving to db" do
+	#   tim = Student.new(:first_name => "Tim", :last_name=>"Licata",:age=>30)
+	#   expect(tim.new_record?).to eq(true)
+	# end
+	# it "should test that save works" do 
+	#   tim = Student.new(:first_name => "Timothy", :last_name=>"Licata",:age=>30)
+	# 	expect(tim.new_record?).to eq(true)
+	# 	tim.save
+	# 	expect(tim.new_record?).to eq(false) 
+	# end
+	# it "should test that save works differently" do 
+	# 	Student.create(:first_name => "Timothy", :last_name=>"Licata",:age=>30)
+	#   tim = Student.find_by_first_name("Timothy")
+	# 	expect(tim).not_to eq(nil) 
+	# end
+end
